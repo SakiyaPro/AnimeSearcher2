440b4122022c64e2e3779ea851d4a9f4b7042e9b
sakiya <sakitan0305@gmail.com>
2022-02-11 16:26:50 +0900

    アニメジャンルとあらすじのスクレイピング完成

--- django/anime_data/views.py
+++ django/anime_data/views.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9015108f29a75a8ac76d2b1f4c6001833e29f0db0b9951c1cf6e50a9aa962d84
-size 14217
+oid sha256:bd45f565d2ff52d5e5f84f5632fdfc8597c6c46a0639a1dc13499467d5048a32
+size 14164
